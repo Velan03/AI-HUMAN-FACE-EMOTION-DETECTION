2d10f560e3b283011404791f9706e1d052fa51d7
velan_sivasankaran <111336249+Velan03@users.noreply.github.com>
2024-04-29 11:59:03 +0530

    Recognition Using LBP_SVM/ Facial_Recognition_Models

--- model.py
+++ model.py
@@ -1,41 +1,104 @@
-# Let us import the Libraries required.
+import cv2
+import os
+import glob
+from skimage import feature
 import numpy as np
-import tensorflow as tf
-
-# To use the model saved in the Json format, We are importing "model_from_json"
-from tensorflow.keras.models import model_from_json
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
 
-class FacialExpressionModel(object):
+class LocalBinaryPatterns:
 
-    """ A Class for Predicting the emotions using the pre-trained Model weights"""
+    def __init__(self, numPoints, radius):
+        # store the number of points and radius
+        self.numPoints = numPoints
+        self.radius = radius
 
-    EMOTIONS_LIST = ["Angry", "Disgust",
-                     "Fear", "Happy",
-                     "Neutral", "Sad",
-                     "Surprise"]
+    def describe(self, image, eps=1e-7):
+        # compute the Local Binary Pattern representation
+        # of the image, and then use the LBP representation
+        # to build the histogram of patterns
+        lbp = feature.local_binary_pattern(image, self.numPoints,
+                                           self.radius, method="uniform")
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=np.arange(0, self.numPoints + 3),
+                                 range=(0, self.numPoints + 2))
+        # normalize the histogram
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+        # return the histogram of Local Binary Patterns
+        return hist
 
-    # Whenever we create an instance of class , these are initialized
-    def __init__(self, model_json_file, model_weights_file):
 
-        # Now Let us load model from JSON file which we created during Training
-        with open(model_json_file, "r") as json_file:
+desc = LocalBinaryPatterns(24, 8)
+img_folder = 'dataset/'
 
-            # Reading the json file and storing it in loaded_model
-            loaded_model_json = json_file.read()
-            self.loaded_model = model_from_json(loaded_model_json)
+labels = []
+array = []
 
-        # Now, Let us load weights into the model
-        self.loaded_model.load_weights(model_weights_file)
+i = 0
+cnt = 0
+mapping = {}
+for dir1 in os.listdir(img_folder):
+    cnt = cnt + 1
+    mapping[cnt] = dir1
 
-    def predict_emotion(self, img):
-        """ It predicts the Emotion using our pre-trained model and returns it """
+    for file in os.listdir(os.path.join(img_folder, dir1)):
 
-        self.preds = self.loaded_model.predict(img)
-        return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
+        image_path = os.path.join(img_folder, dir1, file)
+        image = cv2.imread(image_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        hist = desc.describe(gray)  # get the LBP histogram here.
+        hist = np.array(hist).reshape(-1, 1)
+        hist = hist.T
+        labels.append(cnt)
+        if i == 0:
+            array = np.vstack(hist)
+        else:
+            array = np.vstack([array, hist])
+        i = i + 1
 
-    def return_probabs(self, img):
-        """  It returns the Probabilities of each emotions using pre-trained model """
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+clf = GridSearchCV(
+    SVC(kernel='rbf', class_weight='balanced'), param_grid
+)
+clf = clf.fit(array, labels)
 
-        self.preds = self.loaded_model.predict(img)
-        return self.preds
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+
+        hist = desc.describe(roi_gray)  # get the LBP histogram here.
+        hist = np.array(hist).reshape(-1, 1)
+        hist = hist.T
+        roi_color = img[y:y + h, x:x + w]
+        output = clf.predict(hist)
+        cv2.putText(img, str(mapping[output[0]]), (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 2)
+    cv2.imshow('video', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
